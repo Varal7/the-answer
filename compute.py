05815f4c9ea5c1ef4ee2705ad1dc2cb33528e1e8
Victor Quach <varaal@gmail.com>
2018-07-18 22:41:37 -0400

    Add some time delay

--- compute.py
+++ compute.py
@@ -1,9 +1,14 @@
 import numpy as np
+import time
 
 def compute(query, cb=None, cbu=None):
     cbu = cbu if cbu else lambda x: None
     cb = cb if cb else lambda x: None
-    cbu(30)
+
+    for i in range(100):
+        time.sleep(i/2000)
+        cbu(i)
+
     mu, sigma = 42, 1
     s = round(np.random.normal(mu, sigma))
     cb(s)
